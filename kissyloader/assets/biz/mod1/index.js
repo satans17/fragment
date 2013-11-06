989238428a28aeea91c743a3c6072eb62a89acac
satans17 <satans17@gmail.com>
2013-11-06 23:01:17 +0800

    刮刮乐游戏

--- kissyloader/assets/biz/mod1/index.js
+++ kissyloader/assets/biz/mod1/index.js
@@ -5,14 +5,15 @@
  * Time: ����12:34
  * To change this template use File | Settings | File Templates.
  */
-KISSY.add(function(S){
-    var Node = S.require("node");
+KISSY.add(function(S,Ajax,Node){
     var $=Node.all;
+
+    console.log(Ajax);
 
     return function(data){
         $(data.id).html("<p>mod1 loaded!</p>")
     }
 
 },{
-    requires:["./mod1.css"]
+    requires:["ajax","node","./mod1.css"]
 })
